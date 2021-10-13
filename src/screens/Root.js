1a41993afa685fff14ee397940ce0d43e9729597
MSpilari <mb.spilari@hotmail.com>
2021-10-13 11:56:15 -0300

    Estabelecendo as rotas do app

---
+++ src/screens/Root.js
@@ -0,0 +1,17 @@
+import { Switch, Route } from 'react-router-dom'
+
+import { HomePage } from './HomePage'
+import { RockPaperScissorsLizard } from './RockPaperLizard'
+import { RockPaperScissors } from './RockPaperScissors'
+
+const ScreensRoots = () => {
+	return (
+		<Switch>
+			<Route path='/' exact component={<HomePage />} />
+			<Route path='/original' exact component={<RockPaperScissors />} />
+			<Route path='/bonus' exact component={<RockPaperScissorsLizard />} />
+		</Switch>
+	)
+}
+
+export { ScreensRoots }
