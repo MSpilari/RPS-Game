4e4e5a5771e4c4832ce3d2676cf3f2bf23d78fb1
MSpilari <mb.spilari@hotmail.com>
2021-10-15 12:16:34 -0300

    Estruturando a HomePage

--- src/screens/HomePage/index.jsx
+++ src/screens/HomePage/index.jsx
@@ -1,22 +1,26 @@
-import { Link } from 'react-router-dom'
+import RPSLogo from '../../assets/logo.svg'
+import RPSLSLogo from '../../assets/logo-bonus.svg'
+import { Footer } from '../../components/UI/Footer'
+
+import { HomeWrapper, LogosWrapper, Logo, HomeMain, LinkStyled } from './styles'
+
 const HomePage = () => {
 	return (
-		<div>
-			<header>
-				<h1>Rock Paper Scissors</h1>
-			</header>
-			<main>
-				<Link data-testid='original' to='/original'>
+		<HomeWrapper>
+			<LogosWrapper>
+				<Logo alt='Rock Paper Scissor Logo' src={RPSLogo} />
+				<Logo alt='Rock Paper Scissor Lizard Spock Logo' src={RPSLSLogo} />
+			</LogosWrapper>
+			<HomeMain>
+				<LinkStyled data-testid='original' to='/original'>
 					Play Rock Paper Scissors
-				</Link>
-				<Link data-testid='bonus' to='/bonus'>
+				</LinkStyled>
+				<LinkStyled data-testid='bonus' to='/bonus'>
 					Play Rock Paper Scissors Lizard Spock
-				</Link>
-			</main>
-			<footer>
-				<h6>Feito por MSpilari</h6>
-			</footer>
-		</div>
+				</LinkStyled>
+			</HomeMain>
+			<Footer />
+		</HomeWrapper>
 	)
 }
 
