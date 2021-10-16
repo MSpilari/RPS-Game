6e14b0589f11fbaf72c32c9adb004bbccfbd7b27
MSpilari <mb.spilari@hotmail.com>
2021-10-16 11:56:22 -0300

    Criação do DefaultWrapper

---
+++ src/components/wrappers/Default/index.jsx
@@ -0,0 +1,7 @@
+import { DefaultDiv } from './styles'
+
+const DefaultWrapper = ({ children }) => {
+	return <DefaultDiv>{children}</DefaultDiv>
+}
+
+export { DefaultWrapper }
---
+++ src/components/wrappers/Default/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const DefaultDiv = styled.div`
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	justify-content: center;
+`
+
+export { DefaultDiv }
