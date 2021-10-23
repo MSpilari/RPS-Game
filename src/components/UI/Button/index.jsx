aba0cf00524f1f86d117c17781b835432f7b45d2
MSpilari <mb.spilari@hotmail.com>
2021-10-23 12:52:39 -0300

    Alterando o Logo pelo name

--- src/components/UI/Button/index.jsx
+++ src/components/UI/Button/index.jsx
@@ -1,6 +1,19 @@
+import RockLogo from '../../../assets/icon-rock.svg'
+import PaperLogo from '../../../assets/icon-paper.svg'
+import ScissorLogo from '../../../assets/icon-scissors.svg'
+import LizardLogo from '../../../assets/icon-lizard.svg'
+import SpockLogo from '../../../assets/icon-spock.svg'
 import { ButtonDiv, ButtonStyled } from './styles'
 
-const Button = ({ img, name, ...props }) => {
+const Button = ({ name, ...props }) => {
+	let img = null
+
+	if (name === 'rock') img = RockLogo
+	else if (name === 'paper') img = PaperLogo
+	else if (name === 'scissors') img = ScissorLogo
+	else if (name === 'lizard') img = LizardLogo
+	else img = SpockLogo
+
 	return (
 		<ButtonStyled name={name} {...props}>
 			<ButtonDiv>
--- src/components/UI/PlayerChoices/index.jsx
+++ src/components/UI/PlayerChoices/index.jsx
@@ -1,27 +1,12 @@
 import { Button } from '../../UI/Button'
-import RockIcon from '../../../assets/icon-rock.svg'
-import PaperIcon from '../../../assets/icon-paper.svg'
-import ScissorIcon from '../../../assets/icon-scissors.svg'
 import { PlayerChoicesWrapper } from './styles'
 
 const PlayerChoices = ({ changeStateFunc }) => {
 	return (
 		<PlayerChoicesWrapper>
-			<Button
-				img={RockIcon}
-				name='rock'
-				onClick={() => changeStateFunc('Rock')}
-			/>
-			<Button
-				img={PaperIcon}
-				name='paper'
-				onClick={() => changeStateFunc('Paper')}
-			/>
-			<Button
-				img={ScissorIcon}
-				name='scissors'
-				onClick={() => changeStateFunc('Scissor')}
-			/>
+			<Button name='rock' onClick={() => changeStateFunc('Rock')} />
+			<Button name='paper' onClick={() => changeStateFunc('Paper')} />
+			<Button name='scissors' onClick={() => changeStateFunc('Scissor')} />
 		</PlayerChoicesWrapper>
 	)
 }
