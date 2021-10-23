3fa0f21f3c6db5748c99d95ea74f1db5a1f64352
MSpilari <mb.spilari@hotmail.com>
2021-10-23 12:06:45 -0300

    Alterando uma vírgula para evitar problemas no CSS

--- src/providers/ThemeProvider.js
+++ src/providers/ThemeProvider.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components'
 const allColors = {
 	scissorsGradient: 'hsl(39, 89%, 49%), hsl(40, 84%, 53%)',
 	paperGradient: 'hsl(230, 89%, 62%), hsl(230, 89%, 65%)',
-	rockGradient: 'hsl(349, 71%, 52%),hsl(349, 70%, 56%)',
+	rockGradient: 'hsl(349, 71%, 52%), hsl(349, 70%, 56%)',
 	lizardGradient: 'hsl(261, 73%, 60%), hsl(261, 72%, 63%)',
 	spockGradient: 'hsl(189, 59%, 53%), hsl(189, 58%, 57%)',
 	darkText: 'hsl(229, 25%, 31%)',
