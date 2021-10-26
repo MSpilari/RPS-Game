a90f7efe75ad35fbe20beb664a2227ff54775c08
MSpilari <mb.spilari@hotmail.com>
2021-10-26 17:35:15 -0300

    Criando o context e provider do Score

---
+++ src/providers/ScoreProvider.js
@@ -0,0 +1,15 @@
+import { createContext, useState } from 'react'
+
+const ScoreContext = createContext(0)
+
+const ScoreProvider = ({ children }) => {
+	const [score, setScore] = useState(0)
+
+	return (
+		<ScoreContext.Provider value={{ score, setScore }}>
+			{children}
+		</ScoreContext.Provider>
+	)
+}
+
+export { ScoreProvider, ScoreContext }
