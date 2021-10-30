0944eae427679d5dc528d0ffdac7c2d41cbe5519
MSpilari <mb.spilari@hotmail.com>
2021-10-30 14:21:25 -0300

    Inserindo as sombras no botão

--- src/components/UI/Button/styles.js
+++ src/components/UI/Button/styles.js
@@ -15,6 +15,13 @@ const ButtonStyled = styled.button`
 		else if (name === 'scissors') return 'c'
 		else if (name === 'rock') return 'e'
 	}};
+	box-shadow: 0 10px 0px 0 ${({ theme, name }) => `${theme[name + 'Single']}`};
+	transition: linear 0.4s;
+
+	&:hover {
+		box-shadow: 0 10px 16px 16px
+			${({ theme, name }) => `${theme[name + 'Single']}`};
+	}
 
 	@media screen and (max-width: 570px) {
 		padding: 14px 19px;
@@ -25,6 +32,7 @@ const ButtonDiv = styled.div`
 	border-radius: 100%;
 	background-color: white;
 	padding: 25px 35px;
+	box-shadow: 0 -7px 0 0 rgba(0, 0, 0, 0.6);
 
 	@media screen and (max-width: 570px) {
 		padding: 19px 29px;
