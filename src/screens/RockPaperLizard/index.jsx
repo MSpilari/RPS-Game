431927c46b88eed9d2950d9349c6d656eff05a02
MSpilari <mb.spilari@hotmail.com>
2021-11-03 17:59:35 -0300

    Criando o esqueleto e estilizando a página bonus

--- src/screens/RockPaperLizard/index.jsx
+++ src/screens/RockPaperLizard/index.jsx
@@ -1,5 +1,52 @@
+import { useContext, useState } from 'react'
+
+import { ScoreContext } from '../../providers/ScoreProvider'
+import { DefaultWrapper } from '../../components/wrappers/Default'
+import { Scoreboard } from '../../components/UI/Scoreboard'
+import { Backdrop } from '../../components/UI/Backdrop'
+import { RulesButton } from '../../components/UI/RulesButton'
+import { PlayerChoices } from '../../components/UI/PlayerChoices'
+import { PlayerChose } from '../../components/UI/PlayerChose'
+import { Results } from '../../components/UI/Results'
+import { Footer } from '../../components/UI/Footer'
+
+import { MainWrapper } from './styles'
+
 const RockPaperScissorsLizard = () => {
-	return <h1>Lizard Page</h1>
+	const [playerChoice, setPlayerChoice] = useState(null)
+	const [houseChoice, setHouseChoice] = useState(null)
+	const [openBack, setOpenBack] = useState(false)
+	const { score } = useContext(ScoreContext)
+
+	return (
+		<DefaultWrapper>
+			<Scoreboard score={score} />
+			<MainWrapper>
+				{playerChoice === null && (
+					<PlayerChoices bonus changeStateFunc={setPlayerChoice} />
+				)}
+
+				{playerChoice !== null && houseChoice === null && (
+					<PlayerChose
+						playerPick={playerChoice}
+						handleHousePick={setHouseChoice}
+					/>
+				)}
+
+				{playerChoice !== null && houseChoice !== null && (
+					<Results
+						playerPick={playerChoice}
+						housePick={houseChoice}
+						resetHousePick={setHouseChoice}
+						resetPlayerPick={setPlayerChoice}
+					/>
+				)}
+			</MainWrapper>
+			<Backdrop isOpened={openBack} handleOpenBack={setOpenBack} />
+			<RulesButton onClick={() => setOpenBack(true)} />
+			<Footer />
+		</DefaultWrapper>
+	)
 }
 
 export { RockPaperScissorsLizard }
---
+++ src/screens/RockPaperLizard/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const MainWrapper = styled.main`
+	width: 100%;
+	height: 60vh;
+	display: flex;
+	align-items: center;
+`
+
+export { MainWrapper }
