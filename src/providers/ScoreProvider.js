19b28e1ecbd39a01e0a809318c93e427c6d1b6f0
MSpilari <mb.spilari@hotmail.com>
2021-11-03 12:04:59 -0300

    Mantendo o score em caso de existir

--- src/providers/ScoreProvider.js
+++ src/providers/ScoreProvider.js
@@ -3,7 +3,9 @@ import { createContext, useState } from 'react'
 const ScoreContext = createContext(0)
 
 const ScoreProvider = ({ children }) => {
-	const [score, setScore] = useState(0)
+	const [score, setScore] = useState(
+		Number(sessionStorage.getItem('currentScore')) || 0
+	)
 
 	return (
 		<ScoreContext.Provider value={{ score, setScore }}>
