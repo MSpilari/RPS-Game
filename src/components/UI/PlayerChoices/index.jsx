b14e18a661ab1633c96a1e70f14d298fb80297d1
MSpilari <mb.spilari@hotmail.com>
2021-11-03 17:56:58 -0300

    Inserindo a renderização condicional dos botões

--- src/components/UI/PlayerChoices/index.jsx
+++ src/components/UI/PlayerChoices/index.jsx
@@ -1,12 +1,18 @@
 import { Button } from '../../UI/Button'
 import { PlayerChoicesWrapper } from './styles'
 
-const PlayerChoices = ({ changeStateFunc }) => {
+const PlayerChoices = ({ changeStateFunc, bonus }) => {
 	return (
 		<PlayerChoicesWrapper>
 			<Button name='rock' onClick={() => changeStateFunc('Rock')} />
 			<Button name='paper' onClick={() => changeStateFunc('Paper')} />
 			<Button name='scissors' onClick={() => changeStateFunc('Scissors')} />
+			{bonus && (
+				<>
+					<Button name='lizard' onClick={() => changeStateFunc('Lizard')} />
+					<Button name='spock' onClick={() => changeStateFunc('Spock')} />
+				</>
+			)}
 		</PlayerChoicesWrapper>
 	)
 }
