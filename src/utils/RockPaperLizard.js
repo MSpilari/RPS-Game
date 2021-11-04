164c75a35be4a97bc314796ce0b1008c2f3009ea
MSpilari <mb.spilari@hotmail.com>
2021-11-04 17:41:02 -0300

    Colocando as possibilidades de escolha na função de resultado

--- src/utils/RockPaperLizard.js
+++ src/utils/RockPaperLizard.js
@@ -1,8 +1,15 @@
 const RockPaperLizardLogic = (playerChoice, houseChoice) => {
 	if (
 		(playerChoice === 'Rock' && houseChoice === 'Scissors') ||
+		(playerChoice === 'Rock' && houseChoice === 'Lizard') ||
 		(playerChoice === 'Paper' && houseChoice === 'Rock') ||
-		(playerChoice === 'Scissors' && houseChoice === 'Paper')
+		(playerChoice === 'Paper' && houseChoice === 'Spock') ||
+		(playerChoice === 'Scissors' && houseChoice === 'Paper') ||
+		(playerChoice === 'Scissors' && houseChoice === 'Lizard') ||
+		(playerChoice === 'Lizard' && houseChoice === 'Paper') ||
+		(playerChoice === 'Lizard' && houseChoice === 'Spock') ||
+		(playerChoice === 'Spock' && houseChoice === 'Scissors') ||
+		(playerChoice === 'Spock' && houseChoice === 'Rock')
 	)
 		return 'Player Wins'
 	else if (playerChoice === houseChoice) return 'Draw'
