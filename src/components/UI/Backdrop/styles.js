a5e44a936fd8ca24f37b60c8222bd2cbfe1a16ea
MSpilari <mb.spilari@hotmail.com>
2021-11-04 16:59:53 -0300

    Trocando o bg do wrapper do backdrop

--- src/components/UI/Backdrop/styles.js
+++ src/components/UI/Backdrop/styles.js
@@ -11,11 +11,23 @@ const BackdropWrapper = styled.div`
 `
 
 const RulesWrapper = styled.div`
-	width: 90%;
-	margin: 0 auto;
+	width: 70%;
+	height: 50%;
+	margin: auto;
+	border-radius: 8px;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
+	align-items: center;
+	align-self: center;
+	background: white;
+
+	max-width: 500px;
+
+	@media screen and (max-width: 767px) {
+		width: 90%;
+		margin: 0 auto;
+	}
 `
 
 const CloseButton = styled.button`
@@ -26,6 +38,7 @@ const CloseButton = styled.button`
 	cursor: pointer;
 	display: flex;
 	align-content: center;
+	margin-right: 10px;
 
 	&:hover {
 		background-color: rgba(0, 0, 0, 0.3);
@@ -33,11 +46,11 @@ const CloseButton = styled.button`
 `
 
 const ImageRules = styled.img`
-	width: 480px;
+	width: 300px;
 	margin: 0 auto;
 
 	@media screen and (max-width: 767px) {
-		width: 280px;
+		width: 270px;
 	}
 `
 
